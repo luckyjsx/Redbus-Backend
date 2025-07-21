e8df5f873ecba837db3df8b882c605af418e3747
Laxman Suthar <laxmansuthar365@gmail.com>
2025-07-21 17:52:34 +0530

    improve validate request middleware

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,25 +1,32 @@
 // middleware/validateSchema.ts
 import { Request, Response, NextFunction } from 'express';
-import { ZodSchema } from 'zod';
+import { ZodError, ZodSchema } from 'zod';
 
-type RequestPart = 'body' | 'query' | 'params';
+type RequestPart = 'body' | 'query' | 'params' | 'headers';
 
 function validateRequest(schema: ZodSchema, target: RequestPart = 'body') {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const result = schema.safeParse(req[target]);
+    try {
+      const result = schema.safeParse(req[target]);
 
-    if (!result.success) {
-      res.status(400).json({
-        message: 'Validation failed',
-        errors: result.error.format(),
-      });
-      return;  // <-- make sure to return here
+      if (!result.success) {
+        res.status(400).json({
+          message: 'Validation failed',
+          errors: result.error.format(),
+        });
+        return; // <-- make sure to return here
+      }
+
+      req[target] = result.data;
+      next();
+    } catch (err) {
+      if(err instanceof ZodError){
+        const message = err?.issues.map(e => e.message).join(', ');
+        return next(new Error(`Validation error: ${message}`));
+      }
+      next(err);
     }
-
-    req[target] = result.data;
-    next();
   };
 }
 
 export default validateRequest;
-
